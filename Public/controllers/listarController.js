d086bf6c69a6c5d54002caa06dcb66b021bcf940
Thiago <thiagomorais92@live.com>
2015-07-02 23:29:45 -0300

    corrigindo um simples problema request

--- Public/controllers/listarController.js
+++ Public/controllers/listarController.js
@@ -1,15 +1,5 @@
-app.controller('listarController',['$scope', '$window','$route','$location','$http', function($scope,$window,$route,$location,$http) {
-  
-   $scope.inscritos = $location.search().inscritos;
-    
+app.controller('listarController', ['$scope', '$window', '$route', '$location', '$http', function ($scope, $window, $route, $location, $http) {
+
+    $scope.inscritos = $location.search().inscritos;
+
 }]);
-    
-  
-
-    
-    
-    
-     
-    
-    
-
